ec34c71daf2ed94559bb81a9d8d7eb5af88455a7
zhangfeilong <zhangfeilong@weipass.cn>
2017-04-05 09:48:41 +0800

    update

--- sw.js
+++ sw.js
@@ -1,18 +1,12 @@
 /* ===========================================================
  * sw.js
- * ===========================================================
- * Copyright 2016 @huxpro
- * Licensed under Apache 2.0 
- * Register service worker.
- * ========================================================== */
+ * =========================================================== */
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "henuzfl.top",
 ]
 
 
